e94df82854863fe57cdd054571956a454e5d9f02
unknown <shri.yogi.ext@orange.com>
2022-03-15 02:08:25 +0530

    first push in developmnt

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -1,14 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post
+from .models import  Category, SubCategory, Post
+
+
+class AddCategoryAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+class AddSubCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'status',)
-    list_filter = ("status",)
-    search_fields = ['title', 'content']
+    list_display = ('title', 'slug', 'status', 'category_name', 'subcategory_name',)
+    list_filter = ("title",)
+    search_fields = ['title', 'content',]
     prepopulated_fields = {'slug': ('title',)}
 
-    
-admin.site.register(Post)
+admin.site.register(Category, AddCategoryAdmin)
+admin.site.register(SubCategory, AddSubCategoryAdmin)
+admin.site.register(Post, PostAdmin)
